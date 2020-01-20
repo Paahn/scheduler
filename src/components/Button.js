e8660859a0b50ceb87e3a4ceb5e0c1d65f39d2b5
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-20 15:26:56 -0800

    modified button component and scss, ran tests succesfully

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 import "components/Button.scss";
+var classnames = require('classnames');
 
 
 
 export default function Button(props) {
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+  
    return (
-      <button>{props.children}</button>
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
    );
-}
+ }
+ 
