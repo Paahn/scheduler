f2cbcfc3c33b0e7c4d13a083c85b05b97d4bda32
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-20 14:46:08 -0800

    created button in storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "components/Button.scss";
 
+
+
 export default function Button(props) {
-   return <></>;
+   return (
+      <button>{props.children}</button>
+   );
 }
