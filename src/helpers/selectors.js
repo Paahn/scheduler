1a1c6f1a448952acda942d1cec341520826e3575
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-22 15:58:14 -0800

    made selectors.js and test file, made the getAppointments function and passed the tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+function getAppointmentsForDay(state, day) {
+  const resultDay = state.days.find((thisDay) => {
+    return thisDay.name === day;
+  });
+  const appointmentsId = resultDay ? resultDay.appointments : [];
+  const resultArray = [];
+  appointmentsId.forEach((appointmentId) => {
+    resultArray.push(state.appointments[appointmentId])
+  })
+  return resultArray;
+}
+
+export { getAppointmentsForDay}
