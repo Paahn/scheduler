66094099c97552fafd04a3fa0546fdab56d25e05
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-21 21:48:09 -0800

    added an array with appointments and imported Appointment into Application, stand by for the magic to begin

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import InterviewerList from "./InterviewerList";
 import Header from "../components/Appointment/Header";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -22,12 +23,51 @@ const days = [
   },
 ];
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "5pm",
+    interview: {
+      student: "Arthur Negus",
+      interviewer: { 
+        id: 4,
+         name: "Cohana Roy", 
+         avatar: "https://i.imgur.com/FK8V841.jpg" 
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "9am",
+    interview: {
+      student: "Chuck Schuldiner",
+      interviewer: { 
+        id: 2,
+        name: "Tori Malcolm", 
+        avatar: "https://i.imgur.com/Nmx0Qxo.png" 
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "10am"
+  }
 ];
 
 
@@ -49,9 +89,9 @@ export default function Application(props) {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        <Header />
+        {/* <Header />
         <InterviewerList 
-          interviewers={interviewers}/>
+          interviewers={interviewers}/> */}
       </section>
     </main>
   );
