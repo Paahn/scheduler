4a0a1087686f703e1d80f07cf3e7be956738ee6e
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-22 21:52:04 -0800

    passing test for back()

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,24 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial){
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function useCustomHook() {
+    function action() {}
+
+    return { action };
+  }
+
+  function transition() { /* ... */ }
+  function back() { 
+    setHistory((currentHistory) => {
+      if (history.length > 1) {
+        history.pop();
+      }
+  
+      setMode(prev => history.slice(-1)[0]);
+   })
+  }
+
+  return { mode, transition, back };
 }
