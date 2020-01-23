faee0852b95481f6312ece7d5859b04369b8ef98
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-22 19:59:52 -0800

    add get appointments from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,73 +1,9 @@
 import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-// import InterviewerList from "./InterviewerList";
-// import Header from "../components/Appointment/Header";
 import Appointment from "components/Appointment";
+import getAppointmentsForDay from "../helpers/selectors";
 const axios = require('axios');
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-    interview: {
-      student: "Nandor the Relentless",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg"
-      }
-    }
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "5pm",
-    interview: {
-      student: "Arthur Negus",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "9am",
-    interview: {
-      student: "Chuck Schuldiner",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "10am",
-    interview: {
-      student: "Venus Williams",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  }
-];
 
 
 export default function Application(props) {
@@ -92,6 +28,8 @@ export default function Application(props) {
       })
   }, []);
 
+  const appointments = getAppointmentsForDay(state, state.day);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -107,7 +45,7 @@ export default function Application(props) {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => {
+      {getAppointmentsForDay(state, state.day).map((appointment) => {
           return (
             <Appointment
               key={appointment.id}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,4 +10,4 @@ function getAppointmentsForDay(state, day) {
   return resultArray;
 }
 
-export { getAppointmentsForDay}
+export default { getAppointmentsForDay }
