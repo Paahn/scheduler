a6b89ca414f0c7ca4f6fbe60faa3e09aad5488f5
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-22 19:28:53 -0800

    combine state for day, days, and appointments into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -71,8 +71,12 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
@@ -88,9 +92,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
-            setDay={setDay}
+            days={state.days}
+            day={state.day}
+            setDay={setDays}
           />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
