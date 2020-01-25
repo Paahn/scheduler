af9da6d201b689aefba96df6b437f35cf4ec4c2f
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-24 23:38:05 -0800

    working on edit, still crashing but I'm starting to see the face of a giant duck on my screen so it's time to take a break

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -87,6 +87,14 @@ export default function Appointment(props) {
           />
         )}
 
+        {/* {mode === EDIT && (
+          <Form 
+            name={props.interview.student}
+            interviewer={props.interview.interviewer}
+            onCancel={() => back()}
+            onSave={save}
+          />
+        )} */}
       
     </article>
   )
