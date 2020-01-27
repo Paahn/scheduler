4c96782413f6eae727c4bedbb34d3b692f3fb9c8
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-26 17:19:48 -0800

    refactored my hook to fight the stale state in the transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,28 +4,25 @@ export default function useVisualMode(initial){
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
+  function transition(newMode, replace) {
 
     console.log(history)
     if (replace) {
-      history.pop();
+      setHistory(recentHistory => [...recentHistory.slice(0, recentHistory.length - 1), newMode]);
+    } else {
+      setHistory(recentHistory=> [...recentHistory, newMode]);
     }
 
     setMode(newMode);
-    setHistory([...history, newMode])
   }
 
   function back() { 
-    setHistory((history) => {
-      let newHistory = [...history];
-      if (newHistory.length > 1) {
-        newHistory.pop();
+      if (history.length > 1) {
+        const ancientHistory = history.slice(0, history.length - 1);
+        setMode(ancientHistory[ancientHistory.length - 1]);
+        setHistory(recentHistory => recentHistory.slice(0, history.length - 1));
       }
-      setMode(newHistory[newHistory.length - 1]);
-      return newHistory;
-   })
+   
   }
-
-
-  return { mode, transition, back };
+ return { mode, transition, back };
 }
